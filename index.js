b3f181a321f3ab34a2578d2326e86fcb0e8e3c68
TiktakTheDark <87076511+TiktakTheDark@users.noreply.github.com>
2024-07-01 19:57:43 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -9,3 +9,54 @@ window.onscroll = function() {
   }
   prevScrollpos = currentScrollPos;
 } 
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const elements = document.querySelectorAll('.text');
+
+  const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+          if (entry.isIntersecting) {
+              console.log('Element is visible'); // Log pour vérifier la visibilité
+              const fadingEffectElement = entry.target.querySelector('.fadingEffect');
+              if (fadingEffectElement) {
+                  fadingEffectElement.classList.add('visible');
+                  observer.unobserve(entry.target); // Arrête d'observer après l'animation
+              } else {
+                  console.log('No .fadingEffect element found');
+              }
+          }
+      });
+  });
+
+  elements.forEach(element => {
+      observer.observe(element);
+  });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const elements = document.querySelectorAll('.text');
+
+  const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+          if (entry.isIntersecting) {
+              console.log('Element is visible'); // Log pour vérifier la visibilité
+              const fadingEffectElement = entry.target.querySelector('.fadingEffect2');
+              if (fadingEffectElement) {
+                  fadingEffectElement.classList.add('visible');
+                  observer.unobserve(entry.target); // Arrête d'observer après l'animation
+              } else {
+                  console.log('No .fadingEffect element found');
+              }
+          }
+      });
+  });
+
+  elements.forEach(element => {
+      observer.observe(element);
+  });
+});
